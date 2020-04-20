e6a3c8715e2d70c0970895dac630ed8b0e6e4848
PrzemekO <przemyslaw.oglaza@outlook.com>
2020-04-20 22:28:37 +0200

    Utworzenie klasy Dostawca

---
+++ ToMi.BLL/Dostawca.cs
@@ -0,0 +1,29 @@
+﻿using ToMi.Common;
+
+namespace ToMi.BLL
+{
+    /// <summary>
+    /// Zarządzanie dostawcami od których kupujemy produkty
+    /// </summary>
+    public class Dostawca
+    {
+        public int DostawcaId { get; set; }
+        public string NazwaFirmy { get; set; }
+        public string Email { get; set; }
+
+
+        /// <summary>
+        /// Wysyła wiadomość email z przywitaniem dostawcy
+        /// </summary>
+        /// <param name="wiadomosc"></param>
+        /// <returns></returns>
+        public string WyslijEmailWitamy(string wiadomosc)
+        {
+            var emailService = new EmailService();
+            var temat = "Witaj" + this.NazwaFirmy;
+            var potwierdzenie = emailService.WyslijWiadomosc(temat, wiadomosc, this.Email);
+
+            return potwierdzenie;
+        }
+    }
+}
---
+++ ToMi.Common/EmailService.cs
@@ -0,0 +1,24 @@
+﻿namespace ToMi.Common
+{
+    public class EmailService
+    {
+
+        /// <summary>
+        /// Wysyła wiadomość email
+        /// </summary>
+        /// <param name="temat">Temat wiadomości</param>
+        /// <param name="wiadomosc">Treść wiadomości</param>
+        /// <param name="odbiorca">Adres email odbiorcy</param>
+        /// <returns></returns>
+        public string WyslijWiadomosc(string temat, string wiadomosc, string odbiorca)
+        {
+            // Kod aby wysłać email do odbiorcy
+
+            var potwierdzeni = "Wiadomość wysłana" + temat;
+            var logowanieService = new LogowanieService();
+            logowanieService.Logowanie(potwierdzeni);
+
+            return potwierdzeni;
+        }
+    }
+}
