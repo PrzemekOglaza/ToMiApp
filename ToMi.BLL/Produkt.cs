94547cc37f1d4e30166be99901b16e7eca635214
PrzemekO <przemyslaw.oglaza@outlook.com>
2020-04-29 23:06:41 +0200

    Dodanie nowej klasy Produkt urzycie propfull oraz wykonanie testu jednowstkowego

---
+++ ToMi.BLL/Produkt.cs
@@ -0,0 +1,39 @@
+﻿namespace ToMi.BLL
+{
+    /// <summary>
+    /// Zarządza produktami
+    /// </summary>
+    public class Produkt
+    {
+        private int produktId;
+
+        public int ProduktId
+        {
+            get { return produktId; }
+            set { produktId = value; }
+        }
+
+        private string nazwaProduktu;
+
+        public string NazwaProduktu
+        {
+            get { return nazwaProduktu; }
+            set { nazwaProduktu = value; }
+        }
+
+        private string opis;
+
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+
+        public string PowiedzWitaj()
+        {
+            return "Witaj " + nazwaProduktu + " (" + ProduktId + "): " + Opis;
+        }
+           
+
+    }
+}
