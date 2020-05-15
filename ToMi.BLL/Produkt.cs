a45ef7e84a11b03b2d324a7e1bbeb48bb5c308e7
PrzemekO <przemyslaw.oglaza@outlook.com>
2020-05-15 12:12:16 +0200

    Konstruktory i ich testy jednostkowe

--- ToMi.BLL/Produkt.cs
+++ ToMi.BLL/Produkt.cs
@@ -1,10 +1,26 @@
-﻿namespace ToMi.BLL
+﻿using System;
+
+namespace ToMi.BLL
 {
     /// <summary>
     /// Zarządza produktami
     /// </summary>
     public class Produkt
     {
+        public Produkt()
+        {
+            Console.WriteLine("Produkt został utworzony");
+        }
+
+        public Produkt(int produktId, string nazwaProduktu, string opis) : this()
+        {
+            this.ProduktId = produktId;
+            this.NazwaProduktu = nazwaProduktu;
+            this.Opis = opis;
+            Console.WriteLine("Produkt ma nazwe: " + nazwaProduktu);
+
+        }
+
         private int produktId;
 
         public int ProduktId
